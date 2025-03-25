cc23693cc2ea7338e6d762cc659d164d4ac506f7
Mahmutt <mahmutbaglama98@gmail.com>
2025-03-25 23:45:35 +0300

    Team classı dosyası düzenlendi

--- Models/Team.cs
+++ Models/Team.cs
@@ -1,10 +1,23 @@
-﻿namespace Stadyum.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stadyum.API.Models
 {
     public class Team
     {
+        [Key]
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string CaptainId { get; set; }
 
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } // Takım Adı
+
+        [Required]
+        public int CaptainId { get; set; } // Takım Kaptanı (Player ID)
+
+        [ForeignKey("CaptainId")]
+        public Player Captain { get; set; } // Takım kaptanı ilişkisi
+
+        public ICollection<Player> Players { get; set; } = new List<Player>(); // Takım oyuncuları
     }
 }
