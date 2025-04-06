f590d6bb1e0816a0a3cebfd08c1c45f9a25b86f2
Mahmutt <mahmutbaglama98@gmail.com>
2025-04-07 00:12:08 +0300

    Takım ismi güncelleme işlemleri gerçekleştiriliyor.

--- Models/DTOs/TeamUpdateDTO.cs
+++ Models/DTOs/TeamUpdateDTO.cs
@@ -3,7 +3,7 @@ namespace Stadyum.API.Models.DTOs
 {
     public class TeamUpdateDTO
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public int? CaptainId { get; set; }  // ❗ Nullable, yani kaptan olmayabilir
     }
 }
