7c6d3e9cded3a457e9163bce6167742a0f7f5fd1
Mahmutt <mahmutbaglama98@gmail.com>
2025-04-21 02:49:31 +0300

    oyuncu güncelleme endpointleri yenilendi.

--- Models/DTOs/PlayerUpdateDTO.cs
+++ Models/DTOs/PlayerUpdateDTO.cs
@@ -8,7 +8,7 @@
         public string Position { get; set; } = string.Empty;
         public int SkillLevel { get; set; }
         public float Rating { get; set; }
-        public DateTime CreateAd { get; set; }
+        public DateTime CreateAd { get; set; } = DateTime.Now;
         public int? TeamId { get; set; }
     }
 }
